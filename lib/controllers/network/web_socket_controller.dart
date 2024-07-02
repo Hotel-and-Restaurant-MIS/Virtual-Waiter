7d17f94c4fd6e3169ed8fe5f01b08f4329883a50
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-07-02 16:35:24 +0530

    web_socket_controller.dart is created.

--- lib/controllers/network/web_socket_controller.dart
+++ lib/controllers/network/web_socket_controller.dart
@@ -1,22 +1,45 @@
 import 'package:get/get.dart';
 import 'package:socket_io_client/socket_io_client.dart' as SIO;
+import 'package:virtual_waiter/constant.dart';
 
-class WebSocketController extends GetxController{
-  static WebSocketController instance = Get.find();
+class WebSocketController extends GetxController {
+  static WebSocketController instance = Get.find();   //create instance by using Singleton design pattern
 
   late SIO.Socket _socket;
 
-  WebSocketController._();
+  WebSocketController._();   // private constructor for class
 
-  static Future<WebSocketController> create() async{
+  static Future<WebSocketController> create() async {
     WebSocketController controller = WebSocketController._();
     await controller._initController();
     return controller;
   }
 
-  Future<void> _initController() async{
-    // TODO:fill this out
-
+  Future<void> _initController() async {
+    try {
+      _socket = SIO.io(
+          {kBackendURL},
+          SIO.OptionBuilder()
+              .setTransports(['websocket'])
+              .disableAutoConnect()
+              .build());
+      _socket.onConnect((_) {
+        print("Connected");
+      });
+      _socket.connect();
+      print("Inited");
+    } catch (e) {
+      print("error occurs establishing the websocket");
+      rethrow;
+    }
   }
 
+  Future<void> sendCustomerHelp() async {
+    try{
+      _socket.emit("customerHelpAlert");
+    }catch(e){
+      print("Error sending Customer Help");
+      rethrow;
+    }
+  }
 }
