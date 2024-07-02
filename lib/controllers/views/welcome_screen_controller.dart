0673c82dfe5670e608d19007c265d802d894f775
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-07-03 00:29:05 +0530

    welcome_screen_controller.dart is fully functioning correctly.

--- lib/controllers/views/welcome_screen_controller.dart
+++ lib/controllers/views/welcome_screen_controller.dart
@@ -1,8 +1,20 @@
 import 'package:get/get.dart';
+import 'package:virtual_waiter/controllers/network/web_socket_controller.dart';
 
 
 class WelcomeScreenController extends GetxController {
     static WelcomeScreenController instance = Get.find();
+
+
+    Future<void> tapBtnToRequestHelp() async{
+        try{
+            WebSocketController webSC = WebSocketController.instance;
+            await webSC.sendCustomerHelp();
+        }catch(e){
+            print("error occurs send help request in welcomeScreen");
+            rethrow;
+        }
+    }
 
 
 
