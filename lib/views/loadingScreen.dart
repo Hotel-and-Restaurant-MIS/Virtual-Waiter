01f5cae0a1ae4574acbbfcbd117a2b0b89f74b37
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-07-03 13:52:33 +0530

    before loading the welcomeScreen.dart loadingScreen.dart has run. because controllers have to be initialized.

---
+++ lib/views/loadingScreen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:virtual_waiter/constants/textConstants.dart';
+import 'package:virtual_waiter/controlIerInitializer.dart';
+import 'package:virtual_waiter/views/welcomeScreen.dart';
+
+class Loadingscreen extends StatelessWidget {
+  Future<String> initController() async {
+    try {
+      if (!ControllerInitializer.isInitialized) {
+        await ControllerInitializer.initAllControllers();
+      }
+      return 'Init All Controllers.';
+    } catch (e) {
+      print("Error occurs Loading controllers.");
+      rethrow;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: initController(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return WelcomeScreen();
+          } else if (snapshot.hasError) {
+            return Scaffold(
+              body: SafeArea(
+                child: Text(
+                  'An Unexpected error occurs.',
+                  style: TextConstants.kMainTextStyle(textColour: Colors.red),
+                ),
+              ),
+            );
+          } else {
+            return Scaffold(
+              body: SafeArea(
+                child: Center(
+                  child: LoadingAnimationWidget.threeArchedCircle(
+                    color: Colors.white,
+                    size: 50.0,
+                  ),
+                ),
+              ),
+            );
+          }
+        });
+  }
+}
