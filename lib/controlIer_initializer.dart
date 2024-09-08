a95519197a7f3167aaa694494327bc58fa3fbad6
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-08 15:44:44 +0530

    order_network controller is init

--- lib/controlIer_initializer.dart
+++ lib/controlIer_initializer.dart
@@ -1,19 +1,18 @@
 import 'package:get/get.dart';
 import 'package:virtual_waiter/controllers/data/menu_data_controller.dart';
-import 'package:virtual_waiter/controllers/data/order_data_controller.dart';
 import 'package:virtual_waiter/controllers/data/order_list_data_controller.dart';
-import 'package:virtual_waiter/controllers/data/test_order_data_controller.dart';
+import 'package:virtual_waiter/controllers/data/order_data_controller.dart';
 import 'package:virtual_waiter/controllers/network/menu_data_network_controller.dart';
+import 'package:virtual_waiter/controllers/network/order_network_controller.dart';
 import 'package:virtual_waiter/controllers/views/all_orders_screen/order_list_builder.dart';
 import 'package:virtual_waiter/controllers/views/menuScreen/check_box_controller.dart';
 import 'package:virtual_waiter/controllers/views/menuScreen/menu_grid_builder.dart';
-import 'package:virtual_waiter/controllers/views/order_screen/test_order_state_controller.dart';
+import 'package:virtual_waiter/controllers/views/order_screen/order_state_controller.dart';
 import 'package:virtual_waiter/controllers/views/single_menu_item_screen/smis_state_controller.dart';
 
 import 'controllers/network/web_socket_controller.dart';
 import 'controllers/views/menuScreen/category_button_color_controller.dart';
 import 'controllers/views/menuScreen/category_tab_controller.dart';
-import 'controllers/views/order_screen/order_state_controller.dart';
 import 'controllers/views/welcomeScreen/welcome_screen_controller.dart';
 
 class ControllerInitializer {
@@ -26,9 +25,9 @@ class ControllerInitializer {
       await Get.putAsync(() => WebSocketController.create());
 
       await Get.putAsync(() => MenuDataNetworkController.create());
-
+      Get.put(OrderNetworkController());
       Get.put(OrderListDataController());
-      Get.put(TestOrderDataController());
+      Get.put(OrderDataController());
       Get.put(TestOrderStateController());
 
 
