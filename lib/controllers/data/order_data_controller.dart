01c36593b43c51e679d3c6906a39cb3743fa6f29
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-08 15:48:40 +0530

    addOrderItem method is updated. _calculateTotalAmount method is added.

--- lib/controllers/data/order_data_controller.dart
+++ lib/controllers/data/order_data_controller.dart
@@ -1,8 +1,12 @@
 import 'package:get/get.dart';
+import 'package:get/get_rx/get_rx.dart';
 import 'package:virtual_waiter/controllers/data/order_list_data_controller.dart';
-import 'package:virtual_waiter/controllers/views/order_screen/order_state_controller.dart';
+import 'package:virtual_waiter/controllers/network/order_network_controller.dart';
+import 'package:virtual_waiter/controllers/network/web_socket_controller.dart';
 import 'package:virtual_waiter/enum/order_status.dart';
-import 'package:virtual_waiter/extensions.dart';
+import 'package:virtual_waiter/exception/editable_order_not_exists_exception.dart';
+import 'package:virtual_waiter/exception/item_not_found_exception.dart';
+import 'package:virtual_waiter/exception/multiple_editable_orders_exist_exception.dart';
 import 'package:virtual_waiter/model/order_item.dart';
 import 'package:virtual_waiter/model/order.dart';
 
@@ -10,20 +14,92 @@ class OrderDataController extends GetxController {
   static OrderDataController instance = Get.find();
 
   OrderListDataController _oldc = OrderListDataController.instance;
-  // OrderStateController _osc = OrderStateController.instance;
-  List<OrderItem> _orderItemList = [];
+  OrderNetworkController _onc = OrderNetworkController.instance;
+  WebSocketController _websc = WebSocketController.instance;
 
-  RxList<OrderItem> reactiveOrderItemList = <OrderItem>[]
-      .obs; //this duplicate order List is create for change the quantity at the orderList
+  RxDouble _totalAmount = 0.0.obs;
+  double get totalAmount => _totalAmount.value;
 
+  RxList<OrderItem> _orderItemList = <OrderItem>[].obs;
   List<OrderItem> get orderItemList => _orderItemList;
+
+  static const String _editableOrderConst = 'Editable Order';
 
   void addOrderItem({required OrderItem orderItem}) {
     _orderItemList.add(orderItem);
-    reactiveOrderItemList.add(orderItem);
+    _calculateTotalAmount();
+    if (!_oldc.editableOrderExists()) {
+      Order newOrder = Order(
+        orderItemList: _orderItemList,
+        orderStatus: OrderStatus.Editing,
+        orderTotal: _totalAmount.value,
+        tableId: 5,
+      );
+      _oldc.addOrder(newOrder);
+    } else {
+      try {
+        _oldc.updateEditableOrder(orderTotal: _totalAmount.value);
+      } on EditableOrderNotExistsException catch (e) {
+        rethrow;
+      } on MultipleEditableOrdersExistException catch (e) {
+        rethrow;
+      }
+    }
+    // add to editable
+  }
+
+  void removeItem({required int orderItemId}) {
+    try {
+      _orderItemList.removeWhere((item) => item.orderItemId == orderItemId);
+      _calculateTotalAmount();
+      _oldc.updateEditableOrder(orderTotal: _totalAmount.value);
+      //remove item from editable
+    } catch (e) {
+      throw ItemNotFoundException(
+          message:
+              'Order Item $orderItemId does not exists in OrderDataController');
+    }
   }
 
   bool orderItemExists({required String orderItemId}) {
     return _orderItemList.any((order) => order.orderItemId == orderItemId);
   }
+
+  void _calculateTotalAmount() {
+    double total = 0.0;
+    for (OrderItem item in _orderItemList) {
+      total += item.totalPrice;
+    }
+    _totalAmount.value = total.toPrecision(2);
+  }
+
+  Future<void> sendOrder() async {
+    List<OrderItem> currentItems = [];
+    double orderTotal = 0.0;
+    orderTotal = _totalAmount.value;
+
+    for (OrderItem item in _orderItemList) {
+      currentItems.add(item);
+    }
+
+    Order order = Order(
+      orderItemList: currentItems,
+      orderStatus: OrderStatus.Pending,
+      orderTotal: orderTotal,
+      tableId: 5, //TODO: get from shared preferences
+    );
+    _oldc.removeEditableOrder();
+    //Map<String,dynamic> newOrder = await _onc.addOrder(order: order); // add new order to the db
+    //_oldc.addOrder(Order.fromMap(newOrder));
+    _oldc.addOrder(order);
+    print('odc send() is called');
+    _websc.updateAllOrderList(); //update the order manager app in real time
+    _orderItemList.clear();
+  }
+
+  //TODO: Item update/delete error
+  /*
+  Two items are separately added for order item list. Need to update that
+  When removing a single item from the above case, only one is removed while the other still remains. Need to fix that
+   */
 }
