2008039cb95ee7010f505c5c6169fed147368e99
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-10-06 00:22:25 +0530

    add new method to get the OrderStatus enum by passing string for that enum.

--- lib/enum/order_status.dart
+++ lib/enum/order_status.dart
@@ -1,3 +1,18 @@
 enum OrderStatus{
   Pending,Preparing,Completed,Editing
 }
+
+OrderStatus getOrderStatusFromString(String status) {
+  switch (status) {
+    case 'Pending':
+      return OrderStatus.Pending;
+    case 'Preparing':
+      return OrderStatus.Preparing;
+    case 'Completed':
+      return OrderStatus.Completed;
+    case 'Editing':
+      return OrderStatus.Editing;
+    default:
+      throw Exception('Invalid order status: $status');
+  }
+}
