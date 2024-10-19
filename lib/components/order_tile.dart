68b3c00e3701065d36fb9346248245914fef7db1
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-10-19 13:01:10 +0530

    add orientation builder

--- lib/components/order_tile.dart
+++ lib/components/order_tile.dart
@@ -18,64 +18,72 @@ class OrderTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(15.0),
-      child: Container(
-        decoration: BoxDecoration(
-            color: kBackgroundClour.withOpacity(0.9),
-            borderRadius: BorderRadius.circular(10.0)),
-        // width: 200.0,
-        // height: 80.0,
-        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-        padding: EdgeInsets.all(20.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                orderStatus == OrderStatus.Editing
-                    ? Text(
-                        'My Cart',
-                        style: TextStyle(
-                            fontSize: 22.0, fontWeight: FontWeight.w700,color: Colors.lightGreen),
-                      )
-                    : Text(
-                        'Order No. ${orderId}',
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                Text(
-                  'Total : ${MoneyFormatter(amount: orderTotal).output.nonSymbol}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  'Order Status: ',
-                  style: TextStyle(fontSize: 21.0, fontWeight: FontWeight.w600),
-                ),
-                Obx(
-                  ()=> Text(
-                    ' ${orderStatus.value.name}',
-                    style: TextStyle(
-                        fontSize: 20.0,
+      child: OrientationBuilder(
+        builder: (context, orientation) {
+          double deviceWidth = MediaQuery.of(context).size.width;
+          double deviceHeight = MediaQuery.of(context).size.height;
+
+          return Container(
+            decoration: BoxDecoration(
+                color: kBackgroundClour.withOpacity(0.9),
+                borderRadius: BorderRadius.circular(10.0)),
+            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    orderStatus == OrderStatus.Editing
+                        ? Text(
+                            'My Cart',
+                            style: TextStyle(
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.lightGreen),
+                          )
+                        : Text(
+                            'Order No. ${orderId}',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                    Text(
+                      'Total : ${MoneyFormatter(amount: orderTotal).output.nonSymbol}',
+                      style: TextStyle(
                         fontWeight: FontWeight.w600,
-                        color: orderStatus == OrderStatus.Editing
-                            ? Colors.lightGreen
-                            : kButtonClour),
-                  ),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Order Status: ',
+                      style: TextStyle(
+                          fontSize: 21.0, fontWeight: FontWeight.w600),
+                    ),
+                    Obx(
+                      () => Text(
+                        ' ${orderStatus.value.name}',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w600,
+                            color: orderStatus == OrderStatus.Editing
+                                ? Colors.lightGreen
+                                : kButtonClour),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
